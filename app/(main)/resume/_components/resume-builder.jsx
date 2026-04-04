10d0f376537abbc829aeafdc1fe09bf7eaf265e5
Vibhas_Kr <vibhas.kr.45@gmail.com>
2026-04-04 17:50:43 +0530

    changed the CV theme to dark

--- app/(main)/resume/_components/resume-builder.jsx
+++ app/(main)/resume/_components/resume-builder.jsx
@@ -147,7 +147,7 @@ export default function ResumeBuilder({ initialContent }) {
   };
 
   return (
-    <div data-color-mode="light" className="space-y-4">
+    <div data-color-mode="dark" className="space-y-4">
       <div className="flex flex-col md:flex-row justify-between items-center gap-2">
         <h1 className="font-bold gradient-title text-5xl md:text-6xl">
           Resume Builder
