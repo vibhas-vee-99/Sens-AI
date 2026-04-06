f5b9d02f2ddfe874df72068fc5c7b3e4e7956184
Vibhas_Kr <vibhas.kr.45@gmail.com>
2026-04-06 05:46:30 +0530

    changed the no of interview questions from 10 to 25

--- actions/interview.js
+++ actions/interview.js
@@ -22,7 +22,7 @@ export async function generateQuiz() {
   if (!user) throw new Error("User not found");
 
   const prompt = `
-    Generate 10 technical interview questions for a ${
+    Generate 25 technical interview questions for a ${
       user.industry
     } professional${
     user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
